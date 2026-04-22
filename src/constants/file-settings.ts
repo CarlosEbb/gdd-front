faf0ebbc7a3b3514e95894a65481102f4df09667
KewinBarboza <documentosdigitalesadi@gmail.com>
2026-04-22 14:31:25 -0400

    refactor: remove FICHA paper size and ANCHO margin preset from file settings constants

--- src/constants/file-settings.ts
+++ src/constants/file-settings.ts
@@ -1,6 +1,6 @@
-export const PAPER_SIZES = ['CARTA', 'LEGAL', 'INFORME', 'EJECUTIVO', 'A5', 'B5', 'A4', 'FICHA'] as const
+export const PAPER_SIZES = ['CARTA', 'LEGAL', 'INFORME', 'EJECUTIVO', 'A5', 'B5', 'A4'] as const
 
-export const MARGIN_PRESETS = ['NONE', 'NORMAL', 'ESTRECHO', 'MODERADO', 'ANCHO'] as const
+export const MARGIN_PRESETS = ['NONE', 'NORMAL', 'ESTRECHO', 'MODERADO'] as const
 
 export const ORIENTATION = ['PORTRAIT', 'LANDSCAPE'] as const
 
