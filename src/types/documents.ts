d0b18e748842f9f28525cd970eaeb26cebd0388e
KewinBarboza <documentosdigitalesadi@gmail.com>
2026-05-12 10:30:24 -0400

    feat(documentation): add authentication section and JWT token support to API documentation page

--- src/types/documents.ts
+++ src/types/documents.ts
@@ -110,6 +110,10 @@ export interface GeneratedDocument {
 }
 
 export interface RequestForDocument {
+  uuid_template?: string
+  build_number?: string
+  name_version?: string
+  jwtToken?: string
   variables: Record<string, any>
   elementos: string[]
 }
